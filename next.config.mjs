04d00ff44801a83765e56fdff9ac52448b91d816
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2024-09-04 08:40:47 +0330

    config(main): added nextjs workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
+const isGitHubPages = process.env.DEPLOY_TARGET === "github-pages";
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  assetPrefix: isGitHubPages ? "https://<Account-name>/<Repository-name>" : "",
   basePath: "/nextjs-dashboard",
   output: "export",
   experimental: {
