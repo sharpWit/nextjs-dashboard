9569258bfa9e08bb20f2fc6b69db13ed5ef8a539
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2024-09-06 15:45:07 +0330

    fix(dashboard): updated edit cache

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { unstable_cache } from "next/cache";
 import prisma from "@/app/lib/prisma";
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
@@ -12,51 +11,44 @@ export const metadata: Metadata = {
 // Next.js will invalidate the cache when a
 // request comes in, at most once every 60 seconds.
 // export const revalidate = 3600; // invalidate every hour
-
+// export const dynamic = "force-static";
 // We'll prerender only the params from `generateStaticParams` at build time.
 // If a request comes in for a path that hasn't been generated,
 // Next.js will server-render the page on-demand.
 // export const dynamicParams = true; // or false, to 404 on unknown paths
 
-const getData = unstable_cache(
-  async (id: string) => {
-    return await Promise.all([
-      await prisma.invoice.findUnique({
-        where: { id: Number(id) },
-        select: {
-          id: true,
-          customerId: true,
-          amount: true,
-          status: true,
-        },
-      }),
+const getData = async (id: string) => {
+  return await Promise.all([
+    await prisma.invoice.findUnique({
+      where: { id: Number(id) },
+      select: {
+        id: true,
+        customerId: true,
+        amount: true,
+        status: true,
+      },
+    }),
 
-      await prisma.customer.findMany({
-        select: {
-          id: true,
-          name: true,
-        },
-        orderBy: {
-          name: "asc",
-        },
-      }),
-    ]);
-  },
-  ["invoiceData"], // here we add the userID in the keyParts so Next.js can make separate caches for the different userID's.
-  {
-    tags: ["invoiceData"],
-    revalidate: 3600,
-  }
-);
+    await prisma.customer.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+      orderBy: {
+        name: "asc",
+      },
+    }),
+  ]);
+};
 
-// export const generateStaticParams = async () => {
-//   const invoices = await prisma.invoice.findMany({
-//     select: { id: true },
-//   });
-//   return invoices.map((invoice: any) => ({
-//     id: invoice.id.toString(),
-//   }));
-// };
+export const generateStaticParams = async () => {
+  const invoices = await prisma.invoice.findMany({
+    select: { id: true },
+  });
+  return invoices.map((invoice: any) => ({
+    id: invoice.id.toString(),
+  }));
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id ?? {};
