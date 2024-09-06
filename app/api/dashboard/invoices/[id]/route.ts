9023cb5c583e4ccd5d3467ee46abf75e66ce1187
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2024-09-06 21:53:47 +0330

    updated nextjs.yml

--- app/api/dashboard/invoices/[id]/route.ts
+++ app/api/dashboard/invoices/[id]/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 
+export const dynamic = "force-static";
+
 export async function PATCH(
   req: Request,
   { params: { id } }: { params: { id: string } }
